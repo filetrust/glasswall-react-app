508ee6c19082446e195ef6076f59430aa9d9887f
ahewit <ahewit@essex.ac.uk>
2020-03-31 15:57:59 +0100

    Initial Template

---
+++ src/components/GlasswallLoginLogo/GlasswallLoginLogo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./GlasswallLoginLogo.module.scss";
+
+const GlasswallLoginLogo = () => {
+    return (
+        <div className={styles.logo}></div>
+    );
+};
+
+export default GlasswallLoginLogo;
