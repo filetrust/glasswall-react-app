bb46d148b49b10f6cbb30e511a4c5e9d8112b702
ahewit <ahewit@essex.ac.uk>
2020-04-02 14:59:59 +0100

    Added header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GlasswallLoginLogo from "./components/GlasswallLoginLogo/GlasswallLoginLogo";
 
 import styles from "./App.module.scss";
 
 const App = () => {
+    const [showSplashScreen, setShowSplashScreen] = useState(true);
+
     return (
         <div className={styles.app}>
-            <div className={styles.titleContainer}>
-                <GlasswallLoginLogo />
-            
-                <h1 className={styles.heading}>Glasswall-React-App</h1>
-            </div>            
+            <div className={styles.mainContainer}>
+                {showSplashScreen &&
+                    <>
+                        <div className={styles.splashContainer}>
+                            <GlasswallLoginLogo />
+
+                            <h1 className={styles.heading}>Glasswall-React-App</h1>
+                        </div>
+                    </>
+                }
+
+                {!showSplashScreen &&
+                    <>
+                        <Header menuExpanded={menuExpanded} />
+                        <MainMenu />
+                        <Main  />
+                    </>
+                }
+            </div>
         </div>
     );
 };
